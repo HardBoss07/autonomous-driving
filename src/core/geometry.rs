7b438061654d08bac27f5fa2dbbf0e8a524d9ffb
HardBoss07 <matteo@bosshard.net>
2026-09-02 08:33:00 +0200

    feat(core): add geometry module with LineSegment intersection and custom vec2_serde

---
+++ src/core/geometry.rs
@@ -0,0 +1,51 @@
+use macroquad::prelude::*;
+use serde::{Deserialize, Deserializer, Serialize, Serializer};
+
+pub mod vec2_serde {
+    use super::*;
+
+    pub fn serialize<S>(v: &Vec2, serializer: S) -> Result<S::Ok, S::Error>
+    where
+        S: Serializer,
+    {
+        (v.x, v.y).serialize(serializer)
+    }
+
+    pub fn deserialize<'de, D>(deserializer: D) -> Result<Vec2, D::Error>
+    where
+        D: Deserializer<'de>,
+    {
+        let (x, y) = <(f32, f32)>::deserialize(deserializer)?;
+        Ok(Vec2::new(x, y))
+    }
+}
+
+#[derive(Clone, Copy, Debug, serde::Serialize, serde::Deserialize)]
+pub struct LineSegment {
+    #[serde(with = "vec2_serde")]
+    pub a: Vec2,
+    #[serde(with = "vec2_serde")]
+    pub b: Vec2,
+}
+
+impl LineSegment {
+    pub fn new(a: Vec2, b: Vec2) -> Self {
+        Self { a, b }
+    }
+
+    pub fn intersects(&self, other: &LineSegment) -> bool {
+        let r = self.b - self.a;
+        let s = other.b - other.a;
+        let r_cross_s = r.perp_dot(s);
+
+        if r_cross_s.abs() < 1e-6 {
+            return false;
+        }
+
+        let q_p = other.a - self.a;
+        let t = q_p.perp_dot(s) / r_cross_s;
+        let u = q_p.perp_dot(r) / r_cross_s;
+
+        (0.0..=1.0).contains(&t) && (0.0..=1.0).contains(&u)
+    }
+}
