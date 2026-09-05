dc933a5113945381370046feca990099762e2dc6
HardBoss07 <matteo@bosshard.net>
2026-09-05 22:05:13 +0200

    refactor: removed some monolithic functions into smaller reuseable functions

--- src/app.rs
+++ src/app.rs
@@ -84,157 +84,170 @@ impl App {
             let current_time = get_time();
 
             match self.mode {
-                AppMode::Driving => {
-                    if is_key_pressed(KeyCode::R) {
-                        let (spawn_pos, spawn_heading) =
-                            self.track.start_grid_config.get_anchor_transform(0);
-                        self.car.reset_to_grid(spawn_pos, spawn_heading);
-                        self.camera_target = spawn_pos;
-                        self.skidmark_manager.clear();
-                    }
-
-                    if is_key_pressed(KeyCode::C) {
-                        self.show_checkpoints = !self.show_checkpoints;
-                    }
-
-                    let input = CarInput::read_keyboard();
-                    self.car
-                        .update(&input, &self.config, &self.track, dt, current_time);
-
-                    self.skidmark_manager.update(&self.car, &input, dt);
-
-                    let car_pos = vec2(self.car.pos_x, self.car.pos_y);
-                    let follow_speed = 6.0;
-                    self.camera_target +=
-                        (car_pos - self.camera_target) * (follow_speed * dt).min(1.0);
-
-                    clear_background(Color::new(0.08, 0.08, 0.10, 1.0));
-
-                    set_camera(&Camera2D {
-                        target: self.camera_target,
-                        zoom: vec2(
-                            (2.0 / screen_width()) * self.driving_zoom,
-                            (2.0 / screen_height()) * self.driving_zoom,
-                        ),
-                        ..Default::default()
-                    });
-
-                    let view_half_w = (screen_width() * 0.5) / self.driving_zoom;
-                    let view_half_h = (screen_height() * 0.5) / self.driving_zoom;
-                    let view_bounds = BoundingBox::new(
-                        self.camera_target - vec2(view_half_w, view_half_h),
-                        self.camera_target + vec2(view_half_w, view_half_h),
-                    );
-
-                    debug::draw_grid(64.0, screen_width() * 4.0, screen_height() * 4.0);
-                    track_render::draw_track(
-                        &self.track,
-                        self.grid_texture.as_ref(),
-                        self.wall_texture.as_ref(),
-                        Some(view_bounds),
-                    );
-
-                    self.skidmark_manager.draw();
-
-                    if self.show_checkpoints {
-                        debug::draw_checkpoints(
-                            &self.track.checkpoints,
-                            self.car.timing.next_checkpoint_idx,
-                            Some(view_bounds),
-                        );
-                    }
-
-                    let is_drifting = input.is_drifting();
-                    debug::draw_car(&self.car, self.car_texture.as_ref(), input.handbrake);
-                    debug::draw_drift_indicator(&self.car, is_drifting);
-
-                    set_default_camera();
-
-                    ui::draw_telemetry(&self.car, is_drifting);
-                    ui::draw_tuning(&mut self.config);
-
-                    if ui::draw_editor_toggle_button() {
-                        self.mode = AppMode::TrackEditor;
-                    }
-                }
-                AppMode::TrackEditor => {
-                    let mouse_pos = mouse_position();
-                    let screen_mouse = vec2(mouse_pos.0, mouse_pos.1);
-                    let screen_center = vec2(screen_width() * 0.5, screen_height() * 0.5);
-
-                    let world_mouse =
-                        self.camera_target + (screen_mouse - screen_center) / self.editor_zoom;
-
-                    self.editor.handle_input(
-                        &mut self.track,
-                        self.track_texture.as_ref(),
-                        self.wall_texture.as_ref(),
-                        world_mouse,
-                        screen_mouse,
-                        &mut self.camera_target,
-                        &mut self.editor_zoom,
-                        dt,
-                    );
-
-                    clear_background(Color::new(0.05, 0.05, 0.07, 1.0));
-
-                    set_camera(&Camera2D {
-                        target: self.camera_target,
-                        zoom: vec2(
-                            (2.0 / screen_width()) * self.editor_zoom,
-                            (2.0 / screen_height()) * self.editor_zoom,
-                        ),
-                        ..Default::default()
-                    });
-
-                    let view_half_w = (screen_width() * 0.5) / self.editor_zoom;
-                    let view_half_h = (screen_height() * 0.5) / self.editor_zoom;
-                    let view_bounds = BoundingBox::new(
-                        self.camera_target - vec2(view_half_w, view_half_h),
-                        self.camera_target + vec2(view_half_w, view_half_h),
-                    );
-
-                    debug::draw_grid(64.0, screen_width() * 10.0, screen_height() * 10.0);
-                    track_render::draw_track(
-                        &self.track,
-                        self.grid_texture.as_ref(),
-                        self.wall_texture.as_ref(),
-                        Some(view_bounds),
-                    );
-                    self.editor
-                        .draw_snap_previews(&self.track, world_mouse, self.editor_zoom);
-
-                    set_default_camera();
-
-                    let done = self.editor.update_and_draw_ui(
-                        &mut self.track,
-                        &mut self.editor_zoom,
-                        self.track_texture.as_ref(),
-                        self.wall_texture.as_ref(),
-                    );
-
-                    if self.editor.show_help_overlay {
-                        self.editor.draw_help_overlay();
-                    }
-
-                    if done {
-                        self.track.rebuild_mesh(
-                            5,
-                            self.track_texture.as_ref(),
-                            self.wall_texture.as_ref(),
-                        );
-                        self.mode = AppMode::Driving;
-
-                        let (spawn_pos, heading) =
-                            self.track.start_grid_config.get_anchor_transform(0);
-                        self.car.reset_to_grid(spawn_pos, heading);
-                        self.camera_target = spawn_pos;
-                        self.skidmark_manager.clear();
-                    }
-                }
+                AppMode::Driving => self.update_and_render_driving(dt, current_time),
+                AppMode::TrackEditor => self.update_and_render_editor(dt),
             }
 
             next_frame().await;
         }
+    }
+
+    fn update_and_render_driving(&mut self, dt: f32, current_time: f64) {
+        if is_key_pressed(KeyCode::R) {
+            self.reset_car_to_grid();
+        }
+
+        if is_key_pressed(KeyCode::C) {
+            self.show_checkpoints = !self.show_checkpoints;
+        }
+
+        let input = CarInput::read_keyboard();
+        self.car
+            .update(&input, &self.config, &self.track, dt, current_time);
+
+        self.skidmark_manager.update(&self.car, &input, dt);
+
+        let car_pos = vec2(self.car.pos_x, self.car.pos_y);
+        let follow_speed = 6.0;
+        self.camera_target += (car_pos - self.camera_target) * (follow_speed * dt).min(1.0);
+
+        self.render_driving_scene(&input);
+
+        if ui::draw_editor_toggle_button() {
+            self.mode = AppMode::TrackEditor;
+        }
+    }
+
+    fn reset_car_to_grid(&mut self) {
+        let (spawn_pos, spawn_heading) = self.track.start_grid_config.get_anchor_transform(0);
+        self.car.reset_to_grid(spawn_pos, spawn_heading);
+        self.camera_target = spawn_pos;
+        self.skidmark_manager.clear();
+    }
+
+    fn render_driving_scene(&mut self, input: &CarInput) {
+        clear_background(Color::new(0.08, 0.08, 0.10, 1.0));
+
+        set_camera(&Camera2D {
+            target: self.camera_target,
+            zoom: vec2(
+                (2.0 / screen_width()) * self.driving_zoom,
+                (2.0 / screen_height()) * self.driving_zoom,
+            ),
+            ..Default::default()
+        });
+
+        let view_half_w = (screen_width() * 0.5) / self.driving_zoom;
+        let view_half_h = (screen_height() * 0.5) / self.driving_zoom;
+        let view_bounds = BoundingBox::new(
+            self.camera_target - vec2(view_half_w, view_half_h),
+            self.camera_target + vec2(view_half_w, view_half_h),
+        );
+
+        debug::draw_grid(64.0, screen_width() * 4.0, screen_height() * 4.0);
+        track_render::draw_track(
+            &self.track,
+            self.grid_texture.as_ref(),
+            self.wall_texture.as_ref(),
+            Some(view_bounds),
+        );
+
+        self.skidmark_manager.draw();
+
+        if self.show_checkpoints {
+            debug::draw_checkpoints(
+                &self.track.checkpoints,
+                self.car.timing.next_checkpoint_idx,
+                Some(view_bounds),
+            );
+        }
+
+        let is_drifting = input.is_drifting();
+        debug::draw_car(&self.car, self.car_texture.as_ref(), input.handbrake);
+        debug::draw_drift_indicator(&self.car, is_drifting);
+
+        set_default_camera();
+
+        ui::draw_telemetry(&self.car, is_drifting);
+        ui::draw_tuning(&mut self.config);
+    }
+
+    fn update_and_render_editor(&mut self, dt: f32) {
+        let mouse_pos = mouse_position();
+        let screen_mouse = vec2(mouse_pos.0, mouse_pos.1);
+        let screen_center = vec2(screen_width() * 0.5, screen_height() * 0.5);
+
+        let world_mouse = self.camera_target + (screen_mouse - screen_center) / self.editor_zoom;
+
+        self.editor.handle_input(
+            &mut self.track,
+            self.track_texture.as_ref(),
+            self.wall_texture.as_ref(),
+            world_mouse,
+            screen_mouse,
+            &mut self.camera_target,
+            &mut self.editor_zoom,
+            dt,
+        );
+
+        self.render_editor_scene(world_mouse);
+
+        let done = self.editor.update_and_draw_ui(
+            &mut self.track,
+            &mut self.editor_zoom,
+            self.track_texture.as_ref(),
+            self.wall_texture.as_ref(),
+        );
+
+        if self.editor.show_help_overlay {
+            self.editor.draw_help_overlay();
+        }
+
+        if done {
+            self.exit_editor_to_driving();
+        }
+    }
+
+    fn render_editor_scene(&self, world_mouse: Vec2) {
+        clear_background(Color::new(0.05, 0.05, 0.07, 1.0));
+
+        set_camera(&Camera2D {
+            target: self.camera_target,
+            zoom: vec2(
+                (2.0 / screen_width()) * self.editor_zoom,
+                (2.0 / screen_height()) * self.editor_zoom,
+            ),
+            ..Default::default()
+        });
+
+        let view_half_w = (screen_width() * 0.5) / self.editor_zoom;
+        let view_half_h = (screen_height() * 0.5) / self.editor_zoom;
+        let view_bounds = BoundingBox::new(
+            self.camera_target - vec2(view_half_w, view_half_h),
+            self.camera_target + vec2(view_half_w, view_half_h),
+        );
+
+        debug::draw_grid(64.0, screen_width() * 10.0, screen_height() * 10.0);
+        track_render::draw_track(
+            &self.track,
+            self.grid_texture.as_ref(),
+            self.wall_texture.as_ref(),
+            Some(view_bounds),
+        );
+        self.editor
+            .draw_snap_previews(&self.track, world_mouse, self.editor_zoom);
+
+        set_default_camera();
+    }
+
+    fn exit_editor_to_driving(&mut self) {
+        self.track
+            .rebuild_mesh(5, self.track_texture.as_ref(), self.wall_texture.as_ref());
+        self.mode = AppMode::Driving;
+
+        let (spawn_pos, heading) = self.track.start_grid_config.get_anchor_transform(0);
+        self.car.reset_to_grid(spawn_pos, heading);
+        self.camera_target = spawn_pos;
+        self.skidmark_manager.clear();
     }
 }
--- src/core/car/state.rs
+++ src/core/car/state.rs
@@ -1,7 +1,7 @@
 use crate::core::car::config::CarConfig;
 use crate::core::physics::CarInput;
 use crate::core::timing::TimingState;
-use crate::core::track::Track;
+use crate::core::track::{Track, TrackSegment};
 use macroquad::prelude::{Vec2, vec2};
 use std::f32::consts::PI;
 
@@ -50,91 +50,10 @@ impl CarState {
         let mut drag_coeff = config.drag_coeff;
 
         // 1. Smoothly integrate angular velocity into heading with exponential damping
-        self.heading += self.angular_velocity * dt;
-        self.heading = self.heading.rem_euclid(2.0 * PI);
-        self.angular_velocity *= (1.0f32 - 10.0 * dt).max(0.0);
+        self.integrate_heading(dt);
 
         // 2. Query nearest track segment for kerb & wall collision logic with localized temporal window search
-        if let Some((seg, idx, _dist)) = track.find_nearest_segment_localized(
-            vec2(self.pos_x, self.pos_y),
-            self.nearest_segment_index,
-        ) {
-            self.nearest_segment_index = Some(idx);
-            let offset_vec = vec2(self.pos_x, self.pos_y) - seg.center;
-            let lat_offset = offset_vec.dot(seg.normal);
-            let abs_offset = lat_offset.abs();
-
-            let tarmac_limit = 54.0; // 140px width - (2 * 16px kerb) = 108px tarmac (54px half-width)
-            let wall_limit = 70.0; // 140px track boundary (70px half-width)
-
-            if abs_offset > tarmac_limit && abs_offset <= wall_limit {
-                // Riding 16px Kerb: Rumble slows down top speed slightly but apex clipping improves turn-in rotation
-                drag_coeff *= 1.35;
-                turn_rate *= 1.25;
-            } else if abs_offset > wall_limit {
-                // Wall Collision & Anti-Wall-Riding Physics
-                let wall_normal = if lat_offset > 0.0 {
-                    -seg.normal
-                } else {
-                    seg.normal
-                };
-
-                // Gently clamp car position to wall boundary
-                let clamped_pos = seg.center - wall_normal * (wall_limit - 1.5);
-                self.pos_x = clamped_pos.x;
-                self.pos_y = clamped_pos.y;
-
-                let vel = vec2(self.vel_x, self.vel_y);
-                let speed = vel.length();
-                let v_normal_mag = vel.dot(wall_normal);
-
-                if v_normal_mag < 0.0 {
-                    let v_normal = wall_normal * v_normal_mag;
-                    let v_tangent = vel - v_normal;
-                    let impact_speed = -v_normal_mag;
-
-                    let normal_ratio = if speed > 5.0 {
-                        impact_speed / speed
-                    } else {
-                        0.0
-                    };
-
-                    if normal_ratio < 0.35 {
-                        // SHALLOW IMPACT / PARALLEL SCRAPING:
-                        // Slide smoothly along wall with realistic friction without spinning out
-                        let new_v_tangent = v_tangent * (1.0 - 1.8 * dt).max(0.2);
-                        let new_vel = new_v_tangent;
-
-                        self.vel_x = new_vel.x;
-                        self.vel_y = new_vel.y;
-
-                        // Slight repulsive nudge away from wall so car doesn't stick
-                        self.pos_x += wall_normal.x * 12.0 * dt;
-                        self.pos_y += wall_normal.y * 12.0 * dt;
-                    } else {
-                        // HARSH ANGLE IMPACT:
-                        // Rebound velocity and impart smooth physical angular momentum (torque)
-                        let new_v_normal = -0.25 * v_normal;
-                        let new_v_tangent = 0.55 * v_tangent;
-
-                        let new_vel = new_v_normal + new_v_tangent;
-                        self.vel_x = new_vel.x;
-                        self.vel_y = new_vel.y;
-
-                        // Apply continuous angular velocity torque instead of instant rotation jump
-                        let spin_direction = if v_tangent.dot(seg.tangent) >= 0.0 {
-                            1.0
-                        } else {
-                            -1.0
-                        };
-                        let torque_bias = if lat_offset > 0.0 { -1.0 } else { 1.0 };
-
-                        self.angular_velocity +=
-                            torque_bias * spin_direction * impact_speed * 0.035;
-                    }
-                }
-            }
-        }
+        self.handle_track_boundary_interaction(track, &mut drag_coeff, &mut turn_rate, dt);
 
         let forward = vec2(self.heading.cos(), self.heading.sin());
         let right = vec2(-self.heading.sin(), self.heading.cos());
@@ -187,6 +106,110 @@ impl CarState {
         self.angular_velocity = 0.0;
         self.nearest_segment_index = None;
         self.timing.reset();
+    }
+
+    fn integrate_heading(&mut self, dt: f32) {
+        self.heading += self.angular_velocity * dt;
+        self.heading = self.heading.rem_euclid(2.0 * PI);
+        self.angular_velocity *= (1.0f32 - 10.0 * dt).max(0.0);
+    }
+
+    fn handle_track_boundary_interaction(
+        &mut self,
+        track: &Track,
+        drag_coeff: &mut f32,
+        turn_rate: &mut f32,
+        dt: f32,
+    ) {
+        if let Some((seg, idx, _dist)) = track.find_nearest_segment_localized(
+            vec2(self.pos_x, self.pos_y),
+            self.nearest_segment_index,
+        ) {
+            self.nearest_segment_index = Some(idx);
+            let offset_vec = vec2(self.pos_x, self.pos_y) - seg.center;
+            let lat_offset = offset_vec.dot(seg.normal);
+            let abs_offset = lat_offset.abs();
+
+            let tarmac_limit = 54.0; // 140px width - (2 * 16px kerb) = 108px tarmac (54px half-width)
+            let wall_limit = 70.0; // 140px track boundary (70px half-width)
+
+            if abs_offset > tarmac_limit && abs_offset <= wall_limit {
+                // Riding 16px Kerb: Rumble slows down top speed slightly but apex clipping improves turn-in rotation
+                *drag_coeff *= 1.35;
+                *turn_rate *= 1.25;
+            } else if abs_offset > wall_limit {
+                self.resolve_wall_collision(&seg, lat_offset, wall_limit, dt);
+            }
+        }
+    }
+
+    fn resolve_wall_collision(
+        &mut self,
+        segment: &TrackSegment,
+        lat_offset: f32,
+        wall_limit: f32,
+        dt: f32,
+    ) {
+        // Wall Collision & Anti-Wall-Riding Physics
+        let wall_normal = if lat_offset > 0.0 {
+            -segment.normal
+        } else {
+            segment.normal
+        };
+
+        // Gently clamp car position to wall boundary
+        let clamped_pos = segment.center - wall_normal * (wall_limit - 1.5);
+        self.pos_x = clamped_pos.x;
+        self.pos_y = clamped_pos.y;
+
+        let vel = vec2(self.vel_x, self.vel_y);
+        let speed = vel.length();
+        let v_normal_mag = vel.dot(wall_normal);
+
+        if v_normal_mag < 0.0 {
+            let v_normal = wall_normal * v_normal_mag;
+            let v_tangent = vel - v_normal;
+            let impact_speed = -v_normal_mag;
+
+            let normal_ratio = if speed > 5.0 {
+                impact_speed / speed
+            } else {
+                0.0
+            };
+
+            if normal_ratio < 0.35 {
+                // SHALLOW IMPACT / PARALLEL SCRAPING:
+                // Slide smoothly along wall with realistic friction without spinning out
+                let new_v_tangent = v_tangent * (1.0 - 1.8 * dt).max(0.2);
+                let new_vel = new_v_tangent;
+
+                self.vel_x = new_vel.x;
+                self.vel_y = new_vel.y;
+
+                // Slight repulsive nudge away from wall so car doesn't stick
+                self.pos_x += wall_normal.x * 12.0 * dt;
+                self.pos_y += wall_normal.y * 12.0 * dt;
+            } else {
+                // HARSH ANGLE IMPACT:
+                // Rebound velocity and impart smooth physical angular momentum (torque)
+                let new_v_normal = -0.25 * v_normal;
+                let new_v_tangent = 0.55 * v_tangent;
+
+                let new_vel = new_v_normal + new_v_tangent;
+                self.vel_x = new_vel.x;
+                self.vel_y = new_vel.y;
+
+                // Apply continuous angular velocity torque instead of instant rotation jump
+                let spin_direction = if v_tangent.dot(segment.tangent) >= 0.0 {
+                    1.0
+                } else {
+                    -1.0
+                };
+                let torque_bias = if lat_offset > 0.0 { -1.0 } else { 1.0 };
+
+                self.angular_velocity += torque_bias * spin_direction * impact_speed * 0.035;
+            }
+        }
     }
 
     fn update_steering(
